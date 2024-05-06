792d68175b3d12c548317cc2d0c4b6723b20d7ab
aminulds <aminulds61@gmail.com>
2024-05-06 08:50:52 +0600

    reduxt and localStorage

--- src/app/components/users-list.jsx
+++ src/app/components/users-list.jsx
@@ -1,9 +1,30 @@
+"use client";
 import React from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import {removeAllUser, removeUser} from "@/redux/features/userSlice";
 
 export const UsersList = () => {
+    const usersData = useSelector((data) => data.users);
+
+    const dispatch = useDispatch();
+
   return (
-    <div className="bg-slate-100 p-5 rounded min-w-96">
-        <p className="bg-slate-300 px-4 py-2 rounded">aminul</p>
+    <div className="bg-slate-100 p-5 space-y-3 rounded min-w-96 max-h-96 overflow-y-auto">
+        {usersData.length ?
+            <>
+                <button onClick={() => dispatch(removeAllUser())}
+                        className="px-2 py-1 bg-slate-500 text-white rounded">Remove All
+                </button>
+                {usersData.map(user =>
+                    <div key={user.id} className="bg-slate-300 px-4 py-2 rounded flex items-center justify-between">
+                        <span>{user.name}</span>
+                        <button onClick={() => dispatch(removeUser(user.id))}
+                                className="px-2 py-1 bg-slate-500 text-white rounded">Remove
+                        </button>
+                    </div>
+                )}</> : <p className="text-center">No user listed!</p>
+        }
+
     </div>
   )
 }
--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -1,7 +1,7 @@
-const { createSlice, nanoid } = require("@reduxjs/toolkit");
+const { createSlice, nanoid, current } = require("@reduxjs/toolkit");
 
 const initialState = {
-    users: []
+    users: JSON.parse(localStorage.getItem("users")) ?? []
 }
 
 const userSlice = createSlice({
@@ -9,17 +9,29 @@ const userSlice = createSlice({
     initialState,
     reducers: {
         addUser: (state, action) => {
-            console.log(action);
-
             const data = {
                 id: nanoid(),
                 name: action.payload
             }
-
             state.users.push(data);
+            let userLocalData = JSON.stringify(current(state.users));
+            localStorage.setItem("users", userLocalData);
+        },
+        removeUser: (state, action) => {
+            state.users = state.users.filter((user) => {
+                return user.id !== action.payload;
+            });
+        },
+        removeAllUser: (state, action) => {
+            state.users = [];
+            localStorage.removeItem("users");
         }
     }
 });
 
-export const {addUser} = userSlice.actions;
+export const {
+    addUser,
+    removeUser,
+    removeAllUser
+} = userSlice.actions;
 export default userSlice.reducer;
